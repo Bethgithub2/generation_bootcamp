cf59eadef6dbe61a597934ac0618466ff5db760d
unknown <bethrubio2017@gmail.com>
2022-04-28 16:51:55 -0400

    Ciclos

---
+++ JavaScript/ciclos/assets/js/ejercicios.js
@@ -0,0 +1,67 @@
+//Escribe un ciclo anidado que imprima el siguiente patron
+
+/* 
+ *  
+ * *  
+ * * *  
+ * * * *  
+ * * * * *
+ */
+
+/* for (let i=1; i<=5; i++){
+    for(let j=1; j<=i; j++){
+        document.write('*');
+        //console.log('ciclo hijo \n');
+    }
+   document.write('<br>');
+} */
+
+//Ejercicio 1
+
+//Escribe un ciclo que haga lo siguiente:
+
+//Repetidamente imprime el valor de la variable xValue, disminuyendo por 0.5 su valor cada iteración.
+//El ciclo se mantendrá mientras que el valor de xValue sea positivo.
+
+/* let numeroPositivo = 10;
+do {
+    numeroPositivo = numeroPositivo - 0.5;
+    console.log(numeroPositivo);
+} while (numeroPositivo>0); */
+
+//Ejercicio 2
+//Imprime todos los números impares entre 1 y 100
+//ulises
+
+/* for (let i = 0; i <= 100; i++) {
+    if(i%2!=0) {
+        console.log('El numero ' + i + ' es impar');
+    }
+}
+ */
+
+
+//Ejercicio 3
+//Escribe un ciclo while que imprima de 1 a n dentro de corchetes cuadrados
+//Por ejemplo: si n = 6 imprime [1] [2] [3] [4] [5] [6]
+
+/* let n=6;
+let contador=1;
+while (contador<=n) {
+    document.write("["+contador+"]");
+    contador++;
+} */
+
+
+//Ejercicio 4
+//Escribe un ciclo que calcule la suma de los numeros positivos entre 1 y n
+//ejemplo: n = 5 sum = 15     
+//por que (1 + 2 + 3 + 4 +5 )
+
+let n=15;
+let suma=0;
+
+for(let i=1; i <=n; i++){
+    suma=suma+i;
+}
+console.log(suma);
---
+++ JavaScript/ciclos/assets/js/main.js
@@ -0,0 +1,33 @@
+/* Ciclos */
+/* Realizar tareas que son repetitivas */
+//for
+//for...in
+//forEach
+
+//Ciclo para imprimir "Hola mundo" 10 veces
+//(inicializamos la vaiable que va a llevar el contador;condicion a evaluar, modificador)
+/* for(let i=0;i<10;i++){
+console.log("Hola mundo",i);
+debugger;
+} */
+//iteracion=una vuelta de cada ciclo.
+/* for ciclo coordinado while es para mientras for tiene un limite de iteraciones
+while mientras la condición se cumpla, seguirá ejecutando
+
+ */
+/* while */
+/* let control = 1;
+while (control<=5) {//condicion a observarse
+    console.log('Hola desde un ciclo while',control);
+    control++;//modificador de la variable de control
+} */
+
+//do while
+/* Primero se ejecuta y despues evalua se ejecuta la menos una vez */
+let i = 1
+do {
+    console.log('hola desde un do..while',i);
+    i++;
+    debugger
+} while (i<=5);
+
